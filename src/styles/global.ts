df3d19381b56638c52e2c0ab9a7f6aeb4b2c7040
TheRam_ <43772379+TheRamU@users.noreply.github.com>
2024-03-30 16:46:22 +0800

    Update global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -30,11 +30,4 @@ export default ({ prefixCls }: { prefixCls: string }) => css`
       }
     }
   }
-
-  * {
-    transition-delay: 0s !important;
-    transition-duration: 0s !important;
-    animation-duration: 0s !important;
-    animation-delay: 0s !important;
-  }
 `;
